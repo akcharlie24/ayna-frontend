7f01ee5815b6ea5f3d0de04e2f3936d93057b016
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-06-09 01:39:17 +0530

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,8 @@ const spaceGrotesk = spaceGroteskk({
 });
 
 export const metadata: Metadata = {
-  title: "Dev Overflow",
-  description:
-    " A community-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web developments, mobile app development, algorithms, data structures and more...",
+  title: "SlidesIO AI",
+  description: "Categorize emails using ChatGpt",
   icons: {
     icon: "/assets/images/site-logo.svg",
   },
