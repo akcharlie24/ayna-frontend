301fc82dfcf05d2782a3153cec135f665470853d
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-07-21 01:19:30 +0530

    final touchups with socket.io and route protection remain

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const Home = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/sign-in");
+    }
+  }, []);
   return (
     <div className="flex w-2/3 flex-col items-center justify-center text-4xl italic">
       <p>Open </p>
